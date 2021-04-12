9df96dace9fe8819fb06cc6666df1465711156ed
TAFSIRUDDIN <tafsirtapu04@gmail.com>
2021-04-12 01:54:43 -0700

    final day

---
+++ src/components/Login/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCHX8vxT9A5rXbuDz7XtLC3LOwuC_11TO8",
+    authDomain: "doctor-portal-project-87623.firebaseapp.com",
+    projectId: "doctor-portal-project-87623",
+    storageBucket: "doctor-portal-project-87623.appspot.com",
+    messagingSenderId: "928703892347",
+    appId: "1:928703892347:web:d64e1d86b0e179115a0bde"
+  };
+    export default firebaseConfig;
